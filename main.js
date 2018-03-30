6bf3a508af22534d41897422a4dae21ac6c45f6b
Anderson Pantoja <anderson.pantoja@gmail.com>
2018-03-30 09:56:16 -0300

    versão 4

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-const {app, BrowserWindow, Tray, Menu, globalShortcut} = require('electron');
+const { app, BrowserWindow, Tray, Menu, globalShortcut, dialog } = require('electron');
+const { autoUpdater } = require('electron-updater');
+const log = require('electron-log');
 const url = require('url');
 const path = require('path');
 
@@ -7,6 +9,10 @@ if (process.env.NODE_ENV == 'development'){
 }
 
 app.setAppUserModelId('cbm.ap.gov.br.electron-curso');
+
+autoUpdater.logger = log;
+autoUpdater.logger.transports.file.level = 'info';
+log.info('App starting...');
 
 let mainWindow;
 
@@ -82,4 +88,47 @@ function createWindow(){
     });
 }
 
-app.on('ready', createWindow);
+function sendStatusToWindow(text){
+    log.info(text);
+    const dialogOpts = {
+        type: 'info',
+        buttons: ['ok'],
+        title: 'Atualização do aplicativo',
+        message: 'Detalhes:',
+        detail: text
+    }
+
+    dialog.showMessageBox(dialogOpts);
+}
+
+autoUpdater.on('checking-for-update', () => {
+    sendStatusToWindow('Checking for update...');
+});
+
+autoUpdater.on('update-available', (info) => {
+    sendStatusToWindow('Update available');
+});
+
+autoUpdater.on('update-not-available', (info) => {
+    sendStatusToWindow('Update available');
+});
+
+autoUpdater.on('error', (err) => {
+    sendStatusToWindow('Erro in auto-updater');
+});
+
+autoUpdater.on('download-progress', (progressObj) => {
+    let log_message = "Download speed: " + progressObj.bytesPerSecond;
+    log_message = log_message + ' - Download ' + progressObj.percent + '%';
+    log_message = log_message + ' (' + progressObj.transferred + '/' + progressObj.total;
+    sendStatusToWindow(log_message);
+});
+
+autoUpdater.on('update-downloaded', (info) => {
+    sendStatusToWindow('Update Downloaded');
+});
+
+app.on('ready', function(){
+    autoUpdater.checkForUpdatesAndNotify();
+    createWindow();
+});
